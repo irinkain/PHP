abca9e87d2a93045676e2d683ff53de0e6c63efe
irinkain <irina.inashvili.1@btu.edu.ge>
2020-10-10 16:51:58 +0400

    first exercise

---
+++ myfirstPhpProject/myFirstPhpProject/app/Controllers/post.php
@@ -0,0 +1,8 @@
+function getPublishedPosts() {
+global $conn;
+$sql = "SELECT * FROM posts WHERE published=true";
+$result = mysqli_query($conn, $sql);
+
+$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+return $posts;
+};
